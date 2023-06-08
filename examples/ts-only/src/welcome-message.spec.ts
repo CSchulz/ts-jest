ffc2bb9b9993da7d87f9a28fae90d97694517978
Christian Schulz <kontakt@schulzolutions.de>
2023-06-08 12:39:37 +0200

    add jest types and failing test

--- examples/ts-only/src/welcome-message.spec.ts
+++ examples/ts-only/src/welcome-message.spec.ts
@@ -3,3 +3,8 @@ import { getWelcomeMessage } from './welcome-message'
 test('should show welcome message', () => {
   expect(getWelcomeMessage()).toMatchInlineSnapshot(`"Welcome to ts-jest!!!"`)
 })
+
+test('should not use console for error', () => {
+  const consoleSpy = jest.spyOn(console, 'error');
+  expect(consoleSpy).not.toHaveBeenCalled();
+});
